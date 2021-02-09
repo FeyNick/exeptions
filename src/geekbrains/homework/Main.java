ea593c6f8b71710873dc33b5e6b5dddd495d3eee
FeyNick <feigin.nick2@gmail.com>
2021-02-09 19:22:15 +0300

    the code

--- src/geekbrains/homework/Main.java
+++ src/geekbrains/homework/Main.java
@@ -1,8 +1,37 @@
 package geekbrains.homework;
 
+import java.util.Scanner;
+
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        String[][] arr = new String[4][4];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 4; i++) {
+            for (int k = 0; k < 4; k++) {
+                arr[i][k] = scanner.next();
+            }
+        }
+        System.out.println("Сумма: " + ArrChecker(arr));
+    }
+
+    private static int ArrChecker(String[][] arr) throws Exception {
+        if (arr.length != 4)
+            throw new ArrayIndexOutOfBoundsException("MyArraySizeException");
+        int sum = 0;
+        int i = 0;
+        int k = 0;
+        try {
+            for (i = 0; i < 4; i++) {
+                for (k = 0; k < 4; k++) {
+                    sum += Integer.parseInt(arr[i][k]);
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Exception appeared at " + i + " : " + k);
+            throw new Exception("MyArrayDataException");
+        }
+        return sum;
     }
 }
